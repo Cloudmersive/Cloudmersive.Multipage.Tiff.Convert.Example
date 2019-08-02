e6420559824c1bf08393d544e2b7be2b98f5d63e
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-08-01 21:38:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var CloudmersiveConvertApiClient = require('cloudmersive-convert-api-client');
+var fs = require('fs');
 var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
 
 // Configure API key authorization: Apikey
 var Apikey = defaultClient.authentications['Apikey'];
-Apikey.apiKey = 'YOUR API KEY';
+Apikey.apiKey = 'YOUR-API-KEY-HERE';
 
 
 
@@ -13,14 +14,17 @@ var format1 = "TIFF"; // String | Input file format as a 3+ letter file extensio
 
 var format2 = "PNG"; // String | Output (convert to this format) file format as a 3+ letter file extension.  Supported formats include AAI, ART, ARW, AVS, BPG, BMP, BMP2, BMP3, BRF, CALS, CGM, CIN, CMYK, CMYKA, CR2, CRW, CUR, CUT, DCM, DCR, DCX, DDS, DIB, DJVU, DNG, DOT, DPX, EMF, EPDF, EPI, EPS, EPS2, EPS3, EPSF, EPSI, EPT, EXR, FAX, FIG, FITS, FPX, GIF, GPLT, GRAY, HDR, HEIC, HPGL, HRZ, ICO, ISOBRL, ISBRL6, JBIG, JNG, JP2, JPT, J2C, J2K, JPEG/JPG, JXR, MAT, MONO, MNG, M2V, MRW, MTV, NEF, ORF, OTB, P7, PALM, PAM, PBM, PCD, PCDS, PCL, PCX, PDF, PEF, PES, PFA, PFB, PFM, PGM, PICON, PICT, PIX, PNG, PNG8, PNG00, PNG24, PNG32, PNG48, PNG64, PNM, PPM, PSB, PSD, PTIF, PWB, RAD, RAF, RGB, RGBA, RGF, RLA, RLE, SCT, SFW, SGI, SID, SUN, SVG, TGA, TIFF, TIM, UIL, VIFF, VICAR, VBMP, WDP, WEBP, WPG, X, XBM, XCF, XPM, XWD, X3F, YCbCr, YCbCrA, YUV
 
-var inputFile = Buffer.from(fs.readFileSync("C:\\temp\\inputfile").buffer); // File | Input file to perform the operation on.
+var inputFile = Buffer.from(fs.readFileSync("C:\\temp\\example.tif").buffer); // File | Input file to perform the operation on.
 
 
 var callback = function(error, data, response) {
   if (error) {
     console.error(error);
   } else {
-    console.log('API called successfully. Returned data: ' + data);
+    console.log('API called successfully. Returned data: ' + JSON.stringify( data ));
+    console.log('Pages: ' + data.PageCount);
+    var buf = new Buffer(data.Pages[0].FileBytes, 'base64');
+    fs.writeFileSync('C:\\temp\\page1.png', buf);
   }
 };
 apiInstance.convertImageMultipageImageFormatConvert(format1, format2, inputFile, callback);
